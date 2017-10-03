c6541e855e196b073bb109dccfc1ffeaa02b77b3
unknown <iury-almeida@hotmail.com>
2017-10-03 09:45:57 -0300

    Alterando a connection string do MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 let app = require('./config/express')();
-require('./config/database.js')('mongodb://localhost:27017/treinaweb');
+require('./config/database.js')('mongodb://iury:dbdeathmetal@ds161584.mlab.com:61584/treinaweb');
 
 app.listen(app.get('port'), (req, res) => {
     console.log(`Servidor escutando na porta ${app.get('port')} `);
